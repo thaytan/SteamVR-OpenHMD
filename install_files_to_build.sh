168791706262ac8231ad4188cd1be2df19c25832
Jan Schmidt <jan@centricular.com>
2021-08-29 04:52:23 +1000

    meson: Add file copying infrastructure.
    Make it so that meson will automatically copy the plugin andto
    driver_openhmd/bin/{linux32,linux64,win32,win64} as appropriate
    after compiling it, along with the manifest and resources.
    
    The driver_openhmd dir should be ready for registering straight
    away.

--- install_files_to_build.sh
+++ install_files_to_build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+TARGET_DIR=driver_openhmd
 
-cp driver.vrdrivermanifest build/
-cp -r resources build/
+if [ -x "$TARGET_DIR" ]; then
+    mkdir -p "$TARGET_DIR"
+fi
 
-mkdir -p build/bin/{win64,win32,osx64,osx32,linux64,linux32}
+cp driver.vrdrivermanifest "$TARGET_DIR"/
+cp -r resources "$TARGET_DIR"/
 
-rm -f build/bin/linux64/driver_openhmd.so && cp build/driver_openhmd.so build/bin/linux64 2> /dev/null && \
-  echo "Installed linux plugin!" || echo "Did not install linux plugin!"
+mkdir -p "$TARGET_DIR"/bin/{win64,win32,osx64,osx32,linux64,linux32}
 
-rm -f build/bin/win64/driver_openhmnd.so && cp build/driver_openhmd.dll build/bin/win64 2> /dev/null && echo "Installed windows plugin!" || echo "Did not install windows plugin!"
+rm -f "$TARGET_DIR"/bin/linux64/driver_openhmd.so && cp build/driver_openhmd.so "$TARGET_DIR"/bin/linux64 2> /dev/null && \
+  echo "Installed linux plugin!" || echo "Did not find linux plugin in build/ - not installing"
+
+rm -f "$TARGET_DIR"/bin/win64/driver_openhmd.dll && cp build/driver_openhmd.dll "$TARGET_DIR"/bin/win64 2> /dev/null && echo "Installed Windows 64-bit plugin!" || echo "Did not find Windows plugin in build/ - not installing"
